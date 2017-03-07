920cf6f0d616cd6a07d29ecc0645f64c195f6d3f
Lauren <lauren.e.coombe@gmail.com>
2017-03-07 13:05:18 -0800

    Updated testGraphs.py

--- GraphMatch/testGraphs.py
+++ GraphMatch/testGraphs.py
@@ -2,6 +2,8 @@ import networkx as nx
 import pydotplus
 from networkx.drawing.nx_pydot import write_dot
 
+#NOTE: All this testing is based on playing around with the Networkx documentation, which can be found at https://networkx.readthedocs.io/en/stable/
+
 class Testing: #Because for GraphMatch, we might represent nodes using a class
 
     def __init__(self, x, y):
@@ -28,27 +30,39 @@ G.add_node(V)
 G.add_node(Y)
 G.add_node(H)
 G.add_node(U)
+G.add_node(U)
 
-print nx.is_connected(G)
-print G.has_edge(T, V)
+print "Printing out the nodes of graph G:"
+for node in G.node:
+	print node 
+print 
+
+
+print "Is G connected?: %s" % nx.is_connected(G)
+print "Does G have an edge from nodes T and V? %s" % G.has_edge(T, V)
+print "Now adding edge from T and V"
 G.add_edge(T, V)
-print G.has_edge(T, V)
+print "Now, does G have an edge from nodes T and V? %s" % G.has_edge(T, V)
 
+print "Adding more edges..."
 G.add_edge(V, U)
 G.add_edge(V, Y)
 G.add_edge(Y, H)
 
-print nx.is_connected(G)
+
+print "Is G connected? %s" % nx.is_connected(G)
 
 L = nx.shortest_path(G, T, U)
 print "Length of shortest path: %d" % (len(L)-1) #Subtract 1 because both source and end node included in path
 
+print "The nodes in the shortest path:"
 for l in L:
     print l
 
+#Writing out G to dot file
 write_dot(G, "test.dot")
 
 #Testing 'induced subgraphs'
 X = G.subgraph([T, V, U])
 write_dot(X, "test1.dot")
-print X.has_edge(T, V)
+print "Does X (induced subgraph of G with nodes T, V, U) have an edge from T to V? %s" % X.has_edge(T, V)
